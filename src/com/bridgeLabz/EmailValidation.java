fbfbd6fc2406fb713ebb0285a4954bc8c9c69d65
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 <tusharpatil22700@gmail.com>
2022-11-08 23:26:04 -0800

    initial

--- src/com/bridgeLabz/EmailValidation.java
+++ src/com/bridgeLabz/EmailValidation.java
@@ -1,9 +1,38 @@
 package com.bridgeLabz;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class EmailValidation {
 
 	public static void main(String[] args) {
 		System.out.println("welcome to Email Id Validation program");
+
+		EmailValidation check = new EmailValidation();
+
+		String email = "abc";
+		String email1 = ".abc";
+		String email2 = "1abc";
+		check.validateEmail(email);
+		check.validateEmail(email1);
+		check.validateEmail(email2);
+	}
+
+	public void validateEmail(String input) {
+
+		String email = "^[0-9a-zA-Z]*";
+
+		Pattern pattern = Pattern.compile(email);
+		Matcher match = pattern.matcher(input);
+
+		printResult(match.matches(), input);
+	}
+
+	public void printResult(boolean isOkay, String email) {
+		if (isOkay)
+			System.out.println("\n" + email + "\tVALID");
+		else
+			System.out.println("\n" + email + "\tINVALID");
 
 	}
 
